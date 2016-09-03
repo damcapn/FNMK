26fc9e4d6e92ff22fca8bd707e04d4448a82a854
damcapn <calihistyle101@gmail.com>
2016-09-03 10:21:55 -0600

    added about.php file
    attempting to set a background image.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> YNFT </title>
+
+    <link rel="stylesheet" href="lib/css/style.css">
+
+
+  </head>
+  <body>
+
+<!-- This is the background image. -->
+    <div class="container" id="maverick1.jpg"></div>
+
+
+  </body>
+</html>
