9937c19b0e29c97fe6485f8443e4b83e8f6159fd
damcapn <calihistyle101@gmail.com>
2016-09-11 12:57:00 -0600

    added and removed pix.
    set image information cards and background image. built the css font
    controls.

--- index.php
+++ index.php
@@ -2,17 +2,72 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title> YNFT </title>
-
+    <title> You're Not Funny Tom! </title>
+    <?php require_once("headutils.php");?>
+  <!--  <link rel="short icon" href="lib/images/logo4.ico"/> -->
     <link rel="stylesheet" href="lib/css/style.css">
-
 
   </head>
   <body>
+    <!-- This calls the background-image. -->
+    <div class="container" id="spaceScape.jpg"></div>
 
-<!-- This is the background image. -->
-    <div class="container" id="maverick1.jpg"></div>
+    <header class="text">
+      <p> Da F'N Meme KiNGS<br>
+          PRESENTS:
+    </header>
+    <hr>
+
+    <div class="logo">
+      <image class="YNFTlogo2.png">
+      <div class="image" src="lib/images/YNFTlogo2.png" alt="KOH">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="lib/images/betterThanTwilight.jpg" alt="KOH">
+          <div class="caption">
+            <h3> Original Content Only! </h3>
+            <p>meme<br>mēm<br>noun<br>
+               an element of a culture or system of behavior that may
+               be considered to be passed from one individual to another
+               by nongenetic means, especially imitation.<br>
+               a humorous image, video, piece of text, etc. that is copied
+               (often with slight variations) and spread rapidly by Internet users.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="lib/images/doMyBest.jpg" alt="KOH">
+          <div class="caption">
+            <h3> Laughter is the best medicine... </h3>
+            <p> humor<br>(h)yo͞omər/Submit<br>noun<br>
+                The quality of being amusing or comic, especially as expressed in literature or speech.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="lib/images/rfr.jpg" alt="KOH">
+          <div class="caption">
+            <h3> Sarcasm is the honest to got truth of our subliminal minds...</h3>
+            <p> sarcasm<br>särˌkazəm<br>noun<br>
+                the use of irony to mock or convey contempt,
+                while insuating truth of the mockery.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+<hr>
 
 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   </body>
 </html>
